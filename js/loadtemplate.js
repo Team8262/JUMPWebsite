d3516ebd06300f3e5c8f2073615f7743a754d9e2
WT-MM <wesley.maa@gmail.com>
2022-09-08 18:10:08 -0700

    404 and konami

--- js/loadtemplate.js
+++ js/loadtemplate.js
@@ -23,6 +23,48 @@ function secretFunc(mm){
   newDoc.close();
 }
 
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+    ominousSeyoun()
+	}
+
+};
+
+function ominousSeyoun(){
+  var sImage = document.createElement("div")
+  var audio = new Audio('../assets/audio/ofortuna.mp3');
+  audio.play();
+  setTimeout(function(){
+    sImage.style="position:fixed;top:35%;left:40%;animation:grow 50s linear;width:300px;height:200px;z-index:1031;background: linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('../assets/images/seyoun.jpg');background-size:cover"
+    
+    document.body.appendChild(sImage);
+  }, 500)
+
+  audio.addEventListener("ended", function() 
+  {
+    sImage.remove()
+  });
+
+
+}
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
+
 
 function load(addr, newPage){
   if(newPage){
